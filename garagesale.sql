4c15cad008fd4541ce2f4e07fafdca820896a2af
Matthew <matthewcichy@outlook.com>
2017-05-11 01:01:36 +0100

    Adding freshest database. Ready for PhpMyAdmin import.

--- garagesale.sql
+++ garagesale.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 4.1.14
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 09, 2017 at 06:12 PM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+-- Generation Time: May 11, 2017 at 02:00 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `garagesale`
@@ -26,11 +26,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin_menu`
 --
 
-CREATE TABLE `admin_menu` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `admin_menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `url` varchar(255) NOT NULL,
-  `title` varchar(255) CHARACTER SET utf16 COLLATE utf16_polish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `title` varchar(255) CHARACTER SET utf16 COLLATE utf16_polish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `admin_menu`
@@ -39,9 +40,9 @@ CREATE TABLE `admin_menu` (
 INSERT INTO `admin_menu` (`id`, `url`, `title`) VALUES
 (1, 'products/manage', 'Produkty'),
 (2, 'products/add', '+ Nowy produkt'),
-(3, 'page/manage', 'Strony'),
-(4, 'page/add', '+ Nowa strona'),
-(5, 'headline/edit', 'Edytuj nagłówek'),
+(3, 'news/manage', 'Ogłoszenia'),
+(4, 'news/add', '+ Nowe ogłoszenie'),
+(5, 'headline/edit', 'Nagłówek'),
 (6, 'settings/', 'Ustawienia'),
 (7, 'tools/', '.');
 
@@ -51,12 +52,13 @@ INSERT INTO `admin_menu` (`id`, `url`, `title`) VALUES
 -- Table structure for table `admin_users`
 --
 
-CREATE TABLE `admin_users` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `admin_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `admin_users`
@@ -71,21 +73,13 @@ INSERT INTO `admin_users` (`id`, `email`, `password`, `name`) VALUES
 -- Table structure for table `badges`
 --
 
-CREATE TABLE `badges` (
-  `product_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `badges` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
   `new` tinyint(1) NOT NULL,
   `sold` tinyint(1) NOT NULL,
-  `auction` tinyint(1) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `badges`
---
-
-INSERT INTO `badges` (`product_id`, `new`, `sold`, `auction`) VALUES
-(1, 1, 0, 0),
-(2, 1, 0, 0),
-(3, 1, 0, 0);
+  `auction` tinyint(1) NOT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -93,13 +87,14 @@ INSERT INTO `badges` (`product_id`, `new`, `sold`, `auction`) VALUES
 -- Table structure for table `images`
 --
 
-CREATE TABLE `images` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `ref` text NOT NULL,
   `path` text NOT NULL,
   `thumb_path` text NOT NULL,
-  `main` tinyint(1) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `main` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `images`
@@ -114,12 +109,13 @@ INSERT INTO `images` (`id`, `ref`, `path`, `thumb_path`, `main`) VALUES
 -- Table structure for table `mod_jumbotron`
 --
 
-CREATE TABLE `mod_jumbotron` (
-  `ID` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `mod_jumbotron` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `header` text CHARACTER SET cp1250 COLLATE cp1250_polish_ci NOT NULL,
   `content` text CHARACTER SET cp1250 COLLATE cp1250_polish_ci NOT NULL,
-  `enabled` tinyint(4) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `enabled` tinyint(4) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `mod_jumbotron`
@@ -131,23 +127,27 @@ INSERT INTO `mod_jumbotron` (`ID`, `header`, `content`, `enabled`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pages`
+-- Table structure for table `news`
 --
 
-CREATE TABLE `pages` (
-  `ID` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `news` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `slug` text NOT NULL,
   `title` text NOT NULL,
   `content` text NOT NULL,
-  `hidden` tinyint(1) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `hidden` tinyint(1) NOT NULL,
+  `pinned` tinyint(1) NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Dumping data for table `pages`
+-- Dumping data for table `news`
 --
 
-INSERT INTO `pages` (`ID`, `slug`, `title`, `content`, `hidden`) VALUES
-(8, 'test', 'test', 'page test<br>', 1);
+INSERT INTO `news` (`ID`, `slug`, `title`, `content`, `hidden`, `pinned`, `datetime`) VALUES
+(1, 'psa-title', 'PSA Title', 'This is a Public Service Announcement. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut malesuada sodales nulla, sit amet posuere justo dapibus a. Aliquam pharetra, nulla at mollis facilisis, magna massa pellentesque libero, vitae dignissim tortor tortor id magna. Integer nec erat ipsum. Nunc iaculis felis fringilla nulla hendrerit tempus. Suspendisse placerat sed nibh vitae ultricies. Donec efficitur nisi eu fermentum lacinia. Maecenas vitae libero at metus aliquam facilisis. Integer ultricies tortor eget ex bibendum, ut maximus quam commodo. Aliquam mollis tristique velit, sed lobortis eros rutrum in. Nullam ullamcorper congue gravida. Sed sodales dolor dolor, vitae eleifend urna rutrum a. Integer non ex venenatis, maximus lectus quis, molestie dolor. Phasellus nunc ipsum, fringilla id mattis eu, euismod id leo. Vestibulum egestas velit a pellentesque feugiat. Proin sed scelerisque ligula.<br>', 0, 1, '2017-05-11 00:57:54'),
+(2, 'this-is-another-news', 'This is another news', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut malesuada sodales nulla, sit amet posuere justo dapibus a. Aliquam pharetra, nulla at mollis facilisis, magna massa pellentesque libero, vitae dignissim tortor tortor id magna. Integer nec erat ipsum. Nunc iaculis felis fringilla nulla hendrerit tempus. Suspendisse placerat sed nibh vitae ultricies. Donec efficitur nisi eu fermentum lacinia. Maecenas vitae libero at metus aliquam facilisis. Integer ultricies tortor eget ex bibendum, ut maximus quam commodo. Aliquam mollis tristique velit, sed lobortis eros rutrum in. Nullam ullamcorper congue gravida. Sed sodales dolor dolor, vitae eleifend urna rutrum a. Integer non ex venenatis, maximus lectus quis, molestie dolor. Phasellus nunc ipsum, fringilla id mattis eu, euismod id leo. Vestibulum egestas velit a pellentesque feugiat. Proin sed scelerisque ligula<br>', 0, 0, '2017-05-11 00:58:32');
 
 -- --------------------------------------------------------
 
@@ -155,24 +155,23 @@ INSERT INTO `pages` (`ID`, `slug`, `title`, `content`, `hidden`) VALUES
 -- Table structure for table `products`
 --
 
-CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` text CHARACTER SET cp1250 COLLATE cp1250_polish_ci NOT NULL,
   `description` text CHARACTER SET cp1250 COLLATE cp1250_polish_ci NOT NULL,
   `price` text CHARACTER SET cp1250 COLLATE cp1250_polish_ci NOT NULL,
   `images_ref` text NOT NULL,
   `hidden` tinyint(1) NOT NULL DEFAULT '0',
-  `hidden_notes` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `hidden_notes` text NOT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `products`
 --
 
 INSERT INTO `products` (`id`, `title`, `description`, `price`, `images_ref`, `hidden`, `hidden_notes`) VALUES
-(1, 'test', 'test product<br>', '6.66', '590e5957545eb', 0, '<br>'),
-(2, 'test', 'test product<br>', '6.66', '590e5963c433b', 0, '<br>'),
-(3, 'test123', 'test product<br>', '6.669', '590e5968d7127', 0, 'test 333');
+(3, 'Surface Pro 2 128GB', 'test product description', '6.69', '590e5968d7127', 0, 'this is a hidden note');
 
 -- --------------------------------------------------------
 
@@ -180,38 +179,20 @@ INSERT INTO `products` (`id`, `title`, `description`, `price`, `images_ref`, `hi
 -- Table structure for table `settings`
 --
 
-CREATE TABLE `settings` (
-  `ID` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `settings` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `contactPhone` text NOT NULL,
   `contactEmail` text NOT NULL,
-  `contactExtraNotes` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `contactExtraNotes` text NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `settings`
 --
 
 INSERT INTO `settings` (`ID`, `contactPhone`, `contactEmail`, `contactExtraNotes`) VALUES
-(1, '0123456789', 'abcreative@interia.pl', 'This space can be used to display extra information to the visitors of your website.');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `settings-copy`
---
-
-CREATE TABLE `settings-copy` (
-  `ID` int(11) NOT NULL,
-  `name` text NOT NULL,
-  `value` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `settings-copy`
---
-
-INSERT INTO `settings-copy` (`ID`, `name`, `value`) VALUES
-(1, 'productQuestionEmail', 'abcreative@interia.pl');
+(1, '0123456789', 'example@email.com', 'This space can be used to display extra information to the visitors of your website.');
 
 -- --------------------------------------------------------
 
@@ -219,19 +200,10 @@ INSERT INTO `settings-copy` (`ID`, `name`, `value`) VALUES
 -- Table structure for table `tags`
 --
 
-CREATE TABLE `tags` (
+CREATE TABLE IF NOT EXISTS `tags` (
   `tag_name` text NOT NULL,
   `product_id` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tags`
---
-
-INSERT INTO `tags` (`tag_name`, `product_id`) VALUES
-('main', 1),
-('main', 2),
-('main', 3);
 
 -- --------------------------------------------------------
 
@@ -239,11 +211,12 @@ INSERT INTO `tags` (`tag_name`, `product_id`) VALUES
 -- Table structure for table `user_menu`
 --
 
-CREATE TABLE `user_menu` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user_menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `url` varchar(255) NOT NULL,
-  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `user_menu`
@@ -252,124 +225,6 @@ CREATE TABLE `user_menu` (
 INSERT INTO `user_menu` (`id`, `url`, `title`) VALUES
 (1, '/', 'All products');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin_menu`
---
-ALTER TABLE `admin_menu`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `admin_users`
---
-ALTER TABLE `admin_users`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `badges`
---
-ALTER TABLE `badges`
-  ADD PRIMARY KEY (`product_id`);
-
---
--- Indexes for table `images`
---
-ALTER TABLE `images`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `mod_jumbotron`
---
-ALTER TABLE `mod_jumbotron`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `pages`
---
-ALTER TABLE `pages`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD UNIQUE KEY `id` (`id`);
-
---
--- Indexes for table `settings`
---
-ALTER TABLE `settings`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `settings-copy`
---
-ALTER TABLE `settings-copy`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `user_menu`
---
-ALTER TABLE `user_menu`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin_menu`
---
-ALTER TABLE `admin_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT for table `admin_users`
---
-ALTER TABLE `admin_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `badges`
---
-ALTER TABLE `badges`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `images`
---
-ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `mod_jumbotron`
---
-ALTER TABLE `mod_jumbotron`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `pages`
---
-ALTER TABLE `pages`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
---
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `settings`
---
-ALTER TABLE `settings`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `settings-copy`
---
-ALTER TABLE `settings-copy`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `user_menu`
---
-ALTER TABLE `user_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
